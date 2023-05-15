fef167cd5208c3856a2cdd0965d431095765da9b
Georgy <119348188+GeorgyGogol@users.noreply.github.com>
2023-05-15 12:59:23 +0300

    Добавил статичный модуль Штука с общими методами и функциями для взаимодействия между модулями программы. Уже скомпилировано и все, что нужно - это просто добавить в проект.

---
+++ GeneralLib/GeneralLib.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+#include "GeneralLib.h"
+
+GeneralLib::GeneralLib()
+{
+}
---
+++ GeneralLib/GeneralLib.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "generallib_global.h"
+
+class GENERALLIB_EXPORT GeneralLib
+{
+public:
+    GeneralLib();
+};
